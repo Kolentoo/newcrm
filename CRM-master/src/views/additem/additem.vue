32674b7284f991eb60d95f029a9f4505bdf1a413
kolento <515835543@qq.com>
2019-01-15 09:10:49 +0800

    add pics
    Signed-off-by: kolento <515835543@qq.com>

---
+++ CRM-master/src/views/additem/additem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="additem" id="additem">
+        additem
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
